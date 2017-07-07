4cb18da967643e12916ea0396681b3578f983c6c
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-07 11:45:30 -0500

    main webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var config = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: "style-loader" },// creates style nodes from JS strings
+          {loader: "css-loader"}, // translates CSS into CommonJS
+          {loader: "sass-loader"} // compiles Sass to CSS
+        ]
+      },
+      {test: /\.(js)$/, use: 'babel-loader'}
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
