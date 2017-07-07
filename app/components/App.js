0e80154b72b0d9a172c16c4916f17456ecb7279f
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-07 11:46:00 -0500

    Created a basic component

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Say hello to my little friend!</h1>
+    )
+  }
+}
+
+module.exports = App;
