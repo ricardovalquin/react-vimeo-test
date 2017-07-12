03c79c49655bfcd617dc66626ba2246f588631cd
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-12 14:29:06 -0500

    created some functions on the api

--- app/utils/api.js
+++ app/utils/api.js
@@ -6,12 +6,32 @@ const vimeoApi = {
   PER_PAGE: '12'
 };
 
+function getVideos(category, page) {
+  return axios.get(`${vimeoApi.API_BASE_URL}categories/${category}/videos?page=${page}&per_page=${vimeoApi.PER_PAGE}&access_token=${vimeoApi.ACCESS_TOKEN}`)
+    .then(response => response.data.data);
+}
+
 function searchVideos(query, page) {
   return axios.get(`${vimeoApi.API_BASE_URL}videos/?page=${page}&per_page=${vimeoApi.PER_PAGE}&query=${query}
   &access_token=${vimeoApi.ACCESS_TOKEN}`)
     .then(response => response.data);
 }
 
+function getVideo(category, video) {
+  if (category && video) {
+    return axios.get(`${vimeoApi.API_BASE_URL}categories/${category}/videos/${video}
+    ?access_token=${vimeoApi.ACCESS_TOKEN}`)
+      .then(response => response.data);
+  } else if (!category && video) {
+    return axios.get(`${vimeoApi.API_BASE_URL}videos/${video}
+    ?access_token=${vimeoApi.ACCESS_TOKEN}`)
+      .then(response => response.data);
+  }
+  return null;
+}
+
 module.exports = {
-  searchVideos
+  searchVideos,
+  getVideos,
+  getVideo
 };
