d72aad71f8a5e181082f62448aa1cafad4f56a2b
Ricardo Valencia <ricardovalquin@gmail.com>
2017-07-12 09:35:53 -0500

    Created the header component

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Search from '../Search/Search';
 /* eslint-enable no-unused-vars */
 
 function Header() {
   return (
-    <h1>Header</h1>
+    <div>
+      <div className="header-wp">
+        <h1>Header</h1>
+        <Search />
+      </div>
+    </div>
   );
 }
 
