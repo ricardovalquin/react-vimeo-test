da2156bf431bfbf1509e985a9adb138819015b18
Ricardo Valencia Quintero <ricardovalquin@gmail.com>
2017-07-17 11:24:24 -0500

    Get categories from the API

--- app/utils/api.js
+++ app/utils/api.js
@@ -6,6 +6,17 @@ const vimeoApi = {
   PER_PAGE: '12'
 };
 
+function formatCategories(categories) {
+  if (categories) {
+    return categories.data.data.map((category) => {
+      const uri = category.uri.split('/');
+      return uri[2];
+    });
+  }
+
+  return categories;
+}
+
 function getVideos(category, page) {
   return axios.get(`${vimeoApi.API_BASE_URL}categories/${category}/videos?
   page=${page}&per_page=${vimeoApi.PER_PAGE}&access_token=${vimeoApi.ACCESS_TOKEN}`)
@@ -31,8 +42,20 @@ function getVideo(category, video) {
   return null;
 }
 
+function getCategories() {
+  return axios.get(`${vimeoApi.API_BASE_URL}categories?access_token=${vimeoApi.ACCESS_TOKEN}`)
+    .then(categories => formatCategories(categories));
+}
+
+function getVideoDetails(videoUrl) {
+  return axios.get(`${vimeoApi.API_BASE_URL}${videoUrl}?access_token=${vimeoApi.ACCESS_TOKEN}`)
+    .then(response => response.data);
+}
+
 module.exports = {
   searchVideos,
   getVideos,
-  getVideo
+  getVideo,
+  getCategories,
+  getVideoDetails
 };
